b934e9acf4aa0a82fdfbf0c195eddeccf9471927
MamaniErick <mamanierick64@gmail.com>
2026-05-04 23:06:36 -0300

    feat: Configuracion del navbar con estilos y sistema de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { SlideEventos } from './components/slide-eventos/slide-eventos';
+import { ProductosCarrito } from './components/productos-carrito/productos-carrito';
+import { JuegoMemoria } from './components/juego-memoria/juego-memoria';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'slide-eventos', component: SlideEventos},
+    {path: 'productos', component: ProductosCarrito},
+    {path: 'juego-memoria', component: JuegoMemoria},
+    {path: 'home', component: Home},
+    {path: '',redirectTo: '/home', pathMatch: 'full'}
+];
