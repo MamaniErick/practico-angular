4c90b7b5854daf3a79066cd771f0aa77fa99be87
MamaniErick <mamanierick64@gmail.com>
2026-05-11 15:50:29 -0300

    Agregado el componente a app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { SlideEventos } from './components/slide-eventos/slide-eventos';
 import { ProductosCarrito } from './components/productos-carrito/productos-carrito';
 import { JuegoMemoria } from './components/juego-memoria/juego-memoria';
+import { InscripcionesComponent} from './components/inscripciones/inscripciones';
 import { Home } from './components/home/home';
 
 export const routes: Routes = [
     {path: 'slide-eventos', component: SlideEventos},
     {path: 'productos', component: ProductosCarrito},
     {path: 'juego-memoria', component: JuegoMemoria},
+    {path: 'inscripciones', component: InscripcionesComponent},
     {path: 'home', component: Home},
     {path: '',redirectTo: '/home', pathMatch: 'full'}
 ];
