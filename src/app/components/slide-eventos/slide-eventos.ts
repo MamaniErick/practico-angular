c2f8308987dc06e7691836c3ef0e2e7b7f264e43
Duarte Nicolas <3niico0225@gmail.com>
2026-05-11 19:56:49 -0300

    Agrego el slide eventos

--- src/app/components/slide-eventos/slide-eventos.ts
+++ src/app/components/slide-eventos/slide-eventos.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-slide-eventos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './slide-eventos.html',
   styleUrl: './slide-eventos.css',
 })
-export class SlideEventos {}
+export class SlideEventos {
+  eventos = [
+    { nombre: 'Taller de Yoga', descripcion: 'Relaja tu mente.', img: 'img/eventos/yoga.jpg' },
+    { nombre: 'Charla: Ciberseguridad', descripcion: 'Protege tu entorno digital.', img: 'img/eventos/charla1.jpg' },
+    { nombre: 'Taller de Powerlifting', descripcion: 'Entrena tu fuerza.', img: 'img/eventos/power.jpg' },
+  ];
+  indice: number = 0;
+
+  siguiente() {
+    this.indice = (this.indice + 1) % this.eventos.length;
+  }
+
+  anterior() {
+    this.indice = (this.indice - 1 + this.eventos.length) % this.eventos.length;
+  }
+}
